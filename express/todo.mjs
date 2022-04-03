faa1a3ca497f13f669381864023d330425573fd2
Sayah Reda <reda1998.rs@gmail.com>
2022-04-03 22:44:46 +0100

    set custom middleware

--- express/todo.mjs
+++ express/todo.mjs
@@ -1,16 +1,36 @@
 import express from 'express';
 import morgan from 'morgan';
 import bp from 'body-parser';
+import cors from 'cors';
 const { urlencoded, json } = bp;
 const db = { todo: [] };
 const app = express();
-app.use(urlencoded({ extended: true }));
-app.use(json());
-app.use(morgan('dev'));
-app.get('/todo', (req, res) => {
+
+//those are all midlware
+
+app.use(urlencoded({ extended: true })); //=> to attach to the url string some parametres with ?
+app.use(cors()); //=>used to enable interact with other domains.
+app.use(json()); //=>used to get responsve from body in json
+app.use(morgan('dev')); //this what show all the logs from our server
+
+// implement custom middlware
+// we can call this middlware in 3 way
+/*  1-with app.use to run with each request
+    2- in specified request to run just in that request 
+    3- array in specified request to run servel middlware with that request
+    */
+
+const log = (req, res, next) => {
+  console.log('loggingig');
+  next();
+};
+app.use(log);
+app.get('/todo', log, (req, res) => {
   res.json({ data: db.todo });
 });
-app.post('/todo', (req, res) => {
+app.post('/todo', [log, log], (req, res) => {
+  //=> this function are controllers
+  //todo here is a route
   const newTodo = { id: Date.now, text: req.body.text };
   db.todo.push(newTodo);
   res.json({ data: newTodo });
